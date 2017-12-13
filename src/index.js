511c1350370a42568234054c9025599fb0489428
donkey-hotei <donkey-hotei@cryptolab.net>
2017-12-12 18:09:25 -0800

    update title

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import ChatGalactic from './components/ChatGalactic';
-import subscribeToTimer from './api/client';
+import './api/client';
+
 import './styles/base.scss';
 
 render(
