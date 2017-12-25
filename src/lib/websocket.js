15b7f4ae5bce9c43a37d3264f940fc6e0963180d
donkey-hotei <donkey-hotei@cryptolab.net>
2017-12-24 22:26:52 -0800

    add method waitUntilSocketConnected

--- src/lib/websocket.js
+++ src/lib/websocket.js
@@ -8,31 +8,43 @@ class Websocket {
     }
 
     setupWebsocket(params=defaultParams) {
-        this.logging("Connecting websocket.");
+        if (!params.url) {
+            this.error("Invalid URL for websocket.");
+            return;
+        }
+
+        this.logging("Connecting websocket...");
+
         this.ws = new WebSocket(params.url);
+
         this.ws.onmessage = (data) => {
             this.dispatch(data);
         };
+
         this.ws.onopen = () => {
             this.logging("Websocket connected.");
         };
+
         this.ws.onerror = () => {
-            this.logging("Error occured.");
+            this.error("Error occured.");
         };
+
         this.ws.onclose = () => {
             this.logging('Websocket disconnected.');
+
             if (params.shouldReconnect) {
-                let time = this.generateInterval(this.state.attempts)
+                let interval = this.generateInterval(this.state.attempts)
                 this.timeoutId = setTimeout(() => {
                     this.state.attempts++;
-                    this.ws = new WebSocket(params.url);
                     this.setupWebsocket();
-                }, time);
+                }, interval);
             }
         }
     }
 
-    dispatch() { }
+    dispatch(message) {
+        this.logging("Received Message: " + JSON.stringify(message));
+    }
 
     generateInterval(k) {
         return Math.min(30, (Math.pow(2, k) - 1)) * 1000;
@@ -43,16 +55,44 @@ class Websocket {
             console.log(logline);
     }
 
+    error(logline) {
+        if (this.debug === true)
+            console.error(logline);
+    }
+
+    sendMessage(message) {
+        this.logging("Attempting to send message " +
+                     message)
+        this.waitUntilSocketConnected(this.ws, () => {
+            this.send({
+                Event: "",
+                Body:  message
+            });
+        });
+    }
+
+    waitUntilSocketConnected(ws, cb) {
+        var self = this;
+        setTimeout(() => {
+            if (ws.readyState === 1) {
+                if (cb !== undefined)
+                    cb();
+                return;
+            } else
+                self.waitUntilSocketConnected(ws, cb);
+        }, 5);
+    }
+
     send(data) {
-        this.logging("Sending data.");
         this.ws.send(JSON.stringify(data));
     }
 }
 
 const defaultParams = {
-    url: "ws://d79fd9ff.ngrok.io",
+    url: "ws://5c61f78e.ngrok.io",
     debug: true,
     shouldReconnect: true
 }
+
 const io = new Websocket();
 export default io;
