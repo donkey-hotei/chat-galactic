7f2c4b4b317c06f7fb388d360ff73e863b316e2e
donkey_hotei <donkey-hotei@cryptolab.net>
2018-07-08 17:55:47 -0700

    pull simple websocket example from gorilla's webocket library

--- server/hub.go
+++ server/hub.go
@@ -1,53 +1,53 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
-import (
-    "bytes"
-    "log"
-    "net/http"
-    "time"
-
-    "github.com/gorilla/websocket"
-)
-
+// Hub maintains the set of active clients and broadcasts messages to the
+// clients.
 type Hub struct {
-    // connected peers
-    peers map[*Client]bool
+	// Registered clients.
+	clients map[*Client]bool
 
-    // inbound messages from peers
-    broadcast chan []byte
+	// Inbound messages from the clients.
+	broadcast chan []byte
 
-    // register requests from peers
-    register chan *Client
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
 }
 
-func NewHub() *Hub {
-    return &Hub{
-        peers: make(chan map[*Client]bool),
-        broadcast: make(chan []byte),
-        register: make(chan []byte),
-        unregister: make(chan []byte),
-    }
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
 }
 
 func (h *Hub) run() {
-    for {
-        select {
-        case client := <-h.register:
-            h.clients[client] = true
-        case client := <-h.unregister:
-            if _, ok := h.clients[client]; ok {
-                delete(h.clients, client)
-                close(client.send)
-            }
-        case message := <-h.broadcast:
-            for client := range h.clients {
-                select {
-                case client.send <- message:
-                default:
-                    close(client.send)
-                    delete(h.clients, client)
-                }
-            }
-        }
-    }
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
 }
--- server/main.go
+++ server/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
 )
 
 var addr = flag.String("addr", ":8000", "http service address")
@@ -13,27 +13,28 @@ var addr = flag.String("addr", ":8000", "http service address")
    the server will be running via npm.
 */
 func serveHome(w http.ResponseWriter, r *http.Request) {
-    log.Println(r.URL)
-    if r.URL.Path != "/" {
-        http.Error(w, "Not found", http.StatusNotFound)
-        return
-    }
-    if r.Method != "GET" {
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-        return
-    }
-    http.ServeFile(w, r, "home.html")
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
 }
 
-
 func main() {
-    flag.Parse()
-    hub := newHub()
-    go hub.run()
-    http.HandleFunc("/", serveHome)
-    http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-        serveWs(hub, w, r)
-    })
-    err := http.ListenAndServe(*addr, nil)
-    // TODO
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
